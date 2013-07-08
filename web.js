570e893bbee69bc91b9e026cb69b42140b239650
mbikaprofessionalstudies <mbikaprofessionalstudies@gmail.com>
2013-07-08 06:33:56 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 // #!/usr/bin/env node
-var index1 = require ('./index.html');
-// var index1 = 'Hello World from index.html';
+// var index1 = require ('./index.html');
+ var index1 = 'Hello World from index.html';
 // var index1 = require ('index1');
 var fs = require ('fs');
 var express = require ('express');
