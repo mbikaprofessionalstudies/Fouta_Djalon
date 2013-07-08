2ce72fb3bdfe14ec7d88037bf417aa91eea3da82
mbikaprofessionalstudies <mbikaprofessionalstudies@gmail.com>
2013-07-08 23:16:37 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,20 +1,19 @@
-#!/usr/bin/env node
+//#!/usr/bin/env node
 var fs = require ('fs');
 var express = require ('express');
 var app = express.createServer (express.logger());
-// index1 = fs.readFileSync('./index.html', null);
 //
 // Function index2 to process the message from index.html
 //
-var Outfile = "World.txt";
-var Out = "Hello World from index.html\n";
-  app.get('/', function(request, response) {
-                          response.send(Out);});
+var outfile = "World.txt";
+var out = "Hello World from index.html\n";
+fs.writeFileSync(outfile, out);
+app.get('/', function(request, response) {
+                          response.send(out);});
 // 
 //
 
-  var port = process.env.PORT || 5000;
-  app.listen(port, function() {console.log("listening on" + port);});
-fs.writeFileSync = (Outfile, Out);
-console.log("Script: " + 'web.js' + "\nWrote: " + Out + "TO: " + Outfile);
+var port = process.env.PORT || 5000;
+app.listen(port, function() {console.log("listening on" + port);});
+console.log("Script: " + 'web.js' + "\nWrote: " + out + "TO: " + outfile);
 //
