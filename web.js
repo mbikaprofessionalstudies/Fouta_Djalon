ad29b83309c71cedf2037c4741e77878ec44bf69
mbikaprofessionalstudies <mbikaprofessionalstudies@gmail.com>
2013-07-08 20:45:19 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,5 +1,5 @@
 // #!/usr/bin/env node
- var index1 = require ('./index.html');
+// var index1 = require ('./index.html');
 // var index1 = new Buffer('Hello World from index.html');
 // var index1 = require ('index1');
 var fs = require ('fs');
@@ -10,11 +10,13 @@ var app = express.createServer (express.logger());
 //
 // Function index2 to process the message from index.html
 //
+var Hello = new Buffer('Hello World from index.html', "utf-8");
   app.get('/', function(request, response) {
-                          response.send(index1);});
+                          response.send(Hello.toString('utf-8', 0, Hello.length));});
+// 
 //
   var port = process.env.PORT || 5000;
-  app.listen(port, function() {console.log("listening on" + port + index1);});
+  app.listen(port, function() {console.log("listening on" + port);});
 var outfile = "index.txt";
 fs.writeFileSync = (outfile, index1);
 //
