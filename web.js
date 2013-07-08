2f1635062965dd0cf8fbf1c3bd4dfd5e93c157ff
mbikaprofessionalstudies <mbikaprofessionalstudies@gmail.com>
2013-07-08 03:32:14 +0000

    Second Update web.js

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+#!/usr/bin/env node
+var index1 = require ('./index.html');
+var fs = require ('fs');
+var express = require ('express');
+var app = express.createServer (express.logger());
+//
+// Function index2 to process the message from index.html
+//
+  app.get('/', function(request, response) {
+                          response.send(index1);});
+//
+  var port = process.env.PORT || 5000;
+  app.listen(port, function() {console.log("listening on" + port);});
+//  
+var outfile = "index.txt";
+fs.writeFileSync = (outfile, index1);
