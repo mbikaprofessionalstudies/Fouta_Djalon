2840e5856d264fcd1fa9996c5e16f380a64e5a9c
Ubuntu <ubuntu@ip-172-31-17-107.us-west-2.compute.internal>
2013-07-15 22:21:08 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,19 +1,26 @@
 //#!/usr/bin/env node
 var fs = require ('fs');
 var express = require ('express');
+var htmlfile = "index.html";
+
 var app = express.createServer (express.logger());
+
+app.get('/', function(request, response) {
+    var html = fs.readFilesSync(htmlfile).toString();
+    response.send(html);
+});
 //
 // Function index2 to process the message from index.html
 //
-var outfile = "World.txt";
-var out = "Hello World from index.html\n";
-fs.writeFileSync(outfile, out);
-app.get('/', function(request, response) {
-                          response.send(out);});
+//var outfile = "World.txt";
+//var out = "Hello World from index.html\n";
+//fs.writeFileSync(outfile, out);
+//app.get('/', function(request, response) {
+//                          response.send(out);});
 // 
 //
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {console.log("listening on" + port);});
-console.log("Script: " + 'web.js' + "\nWrote: " + out + "TO: " + outfile);
+var port = process.env.PORT || 8080;
+app.listen(port, function() {console.log("listening on " + port);});
+//console.log("Script: " + 'web.js' + "\nWrote: " + out + "TO: " + outfile);
 //
